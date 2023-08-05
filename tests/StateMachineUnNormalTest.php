8e78afceec8e1a64d217d97b97a11d20ae02da7a
jiaweipan <1028244674@qq.com>
2023-08-05 10:58:02 +0800

    Add unnormal tests

---
+++ tests/StateMachineUnNormalTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DDDPHP\StateMachine\Tests;
+
+use DDDPHP\StateMachine\ActionInterface;
+use DDDPHP\StateMachine\Builder\StateMachineBuilderFactory;
+use DDDPHP\StateMachine\ConditionInterface;
+use DDDPHP\StateMachine\Impl\StateMachineException;
+use PHPUnit\Framework\TestCase;
+
+final class StateMachineUnNormalTest extends TestCase
+{
+    private string $context = 'operator:frank, entityId:123465';
+
+    public function testConditionNotMeet(){
+        $builder = StateMachineBuilderFactory::create();
+        $builder->externalTransition()
+            ->from(StateMachineTest::STATE1)
+            ->to(StateMachineTest::STATE2)
+            ->on(StateMachineTest::EVENT1)
+            ->when($this->checkConditionFalse())
+            ->perform($this->doAction());
+
+        $stateMachine = $builder->build("NotMeetConditionMachine");
+        $target = $stateMachine->fire(StateMachineTest::STATE1, StateMachineTest::EVENT1, $this->context);
+        $this->assertEquals(StateMachineTest::STATE1, $target);
+    }
+
+    public function testDuplicatedTransition(){
+        $this->expectException(StateMachineException::class);
+        $builder = StateMachineBuilderFactory::create();
+        $builder->externalTransition()
+            ->from(StateMachineTest::STATE1)
+            ->to(StateMachineTest::STATE2)
+            ->on(StateMachineTest::EVENT1)
+            ->when($this->checkCondition())
+            ->perform($this->doAction());
+
+        $builder->externalTransition()
+            ->from(StateMachineTest::STATE1)
+            ->to(StateMachineTest::STATE2)
+            ->on(StateMachineTest::EVENT1)
+            ->when($this->checkCondition())
+            ->perform($this->doAction());
+    }
+
+    public function testDuplicateMachine(){
+        $this->expectException(StateMachineException::class);
+        $builder = StateMachineBuilderFactory::create();
+        $builder->externalTransition()
+            ->from(StateMachineTest::STATE1)
+            ->to(StateMachineTest::STATE2)
+            ->on(StateMachineTest::EVENT1)
+            ->when($this->checkCondition())
+            ->perform($this->doAction());
+
+        $builder->build("DuplicatedMachine");
+        $builder->build("DuplicatedMachine");
+    }
+    private function checkConditionFalse(): ConditionInterface
+    {
+        return new class () implements ConditionInterface {
+            public function isSatisfied($context): bool
+            {
+                return false;
+            }
+            public function name(): string
+            {
+                return 'conditionFalse';
+            }
+        };
+    }
+
+    private function checkCondition(): ConditionInterface
+    {
+        return new class () implements ConditionInterface {
+            public function isSatisfied($context): bool
+            {
+                return true;
+            }
+            public function name(): string
+            {
+                return 'condition';
+            }
+        };
+    }
+
+    private function doAction(): ActionInterface
+    {
+        return new class () implements ActionInterface {
+            public function execute($from, $to, $event, $context): void
+            {
+                echo $context . " from:" . $from . " to:" . $to . " on:" . $event;
+            }
+        };
+    }
+}
